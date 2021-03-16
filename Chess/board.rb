255b15ce1c1f1f16c51cdcc67502bba77c1ee13a
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-16 15:36:13 -0400

    created board

--- Chess/board.rb
+++ Chess/board.rb
@@ -1,1 +1,42 @@
+require_relative "./null_pieces.rb"
+require_relative "./piece.rb"
 
+class Board
+    SETUP = [0, 0..(0, 7)]
+    attr_reader :rows
+
+    def initialize
+        @rows = Array.new(8) { Array.new(8) }
+    end
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos, piece)
+        row, col = pos
+        @rows[row][col] = piece
+    end
+
+    def move_piece(start_pos, end_pos)
+        raise "there is no piece at start_pos" 
+        raise "this piece cannot move to end_pos" 
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
