0034a77cddf1b40382495f67f8e2b25d523c7d6b
wzheng10 <wzheng100@gmail.com>
2021-03-16 14:37:20 -0700

    Some Progress

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,1 +1,38 @@
 
+class Piece
+
+    attr_reader :color, :board
+    attr_accessor :pos
+    
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+
+    end
+
+    def to_s
+        
+    end
+
+    def empty?
+        
+    end
+
+    def valid_moves
+
+    end
+
+    def pos=(val)
+        @pos = val
+    end
+
+    def Symbol
+
+    end
+
+    private
+    def move_into_check(end_pos)
+
+    end
+end
