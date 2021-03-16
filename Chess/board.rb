d9e0d0217001e4ac3610f01afb5abd42c3a7867c
wzheng10 <wzheng100@gmail.com>
2021-03-16 13:16:21 -0700

    Board Setup

--- Chess/board.rb
+++ Chess/board.rb
@@ -2,11 +2,12 @@ require_relative "./null_pieces.rb"
 require_relative "./piece.rb"
 
 class Board
-    SETUP = [0, 0..(0, 7)]
     attr_reader :rows
 
     def initialize
-        @rows = Array.new(8) { Array.new(8) }
+        empty_piece = "_"
+        fill_piece = "*"
+        @rows = Array.new(8) { Array.new(8, empty_piece)}
     end
 
     def [](pos)
@@ -20,12 +21,39 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
-        raise "there is no piece at start_pos" 
-        raise "this piece cannot move to end_pos" 
+        raise "there is no piece at start_pos" if self.start_pos.empty?
+        
+        raise "this piece cannot move to end_pos" if !self.end_pos.empty?
+
+    end
+
+    def fill_piece
+        (0...2).each do |row|
+            (0...8).each do |col|
+                @row[row][col] = fill_piece
+            end
+        end
+
+        (6...8).each do |row|
+            (0...8).each do |row|
+                @row[row][col] = fill_piece
+            end
+        end
     end
 
 
-
+#     @rows=
+#     [["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"],
+#      ["_", "_", "_", "_", "_", "_", "_", "_"]]>
+#   [3] pry(main)> b.fill_piece
+#   NoMethodError: undefined method `[]' for nil:NilClass
+#   from board.rb:33:in `block (2 levels) in fill_piece'
 
 
 
