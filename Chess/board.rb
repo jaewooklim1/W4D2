66582e8944869e3174aa18c97173061c253670ab
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-16 17:09:13 -0400

    completed phase 1

--- Chess/board.rb
+++ Chess/board.rb
@@ -7,39 +7,59 @@ class Board
     def initialize
         empty_piece = "_"
         fill_piece = "*"
-        @rows = Array.new(8) { Array.new(8, empty_piece)}
+        @grid = Array.new(8) { Array.new(8, empty_piece)}
     end
 
     def [](pos)
         row, col = pos
-        @rows[row][col]
+        @grid[row][col]
     end
 
     def []=(pos, piece)
         row, col = pos
-        @rows[row][col] = piece
+        @grid[row][col] = piece
     end
 
     def move_piece(start_pos, end_pos)
-        raise "there is no piece at start_pos" if self.start_pos.empty?
+
+        x, y = end_pos
+        a, b = start_pos
+
+        raise "there is no piece at start_pos" if @grid[a][b] == "_" || !valid_pos?(start_pos)
         
-        raise "this piece cannot move to end_pos" if !self.end_pos.empty?
+        raise "this piece cannot move to end_pos" if @grid[x][y] == "*" || !valid_pos?(end_pos)
+
+        @grid[x][y] = "*" 
+        @grid[a][b] = "_"
+
+            #         [12] pry(main)> b.move_piece([10, 10], [3, 0])
+            #           NoMethodError: undefined method `[]' for nil:NilClass
+            #            from board.rb:28:in `move_piece'
 
     end
 
     def fill_piece
         (0...2).each do |row|
             (0...8).each do |col|
-                @row[row][col] = fill_piece
+                @grid[row][col] = "*"
             end
         end
 
         (6...8).each do |row|
-            (0...8).each do |row|
-                @row[row][col] = fill_piece
+            (0...8).each do |col|
+                @grid[row][col] = "*"
             end
         end
     end
+
+    def valid_pos?(pos)
+        x, y = pos
+        (0..7).include?(x) && (0..7).include?(y)
+    end
+
+
+
+  
 
 
 #     @rows=
