62160176a0ad06c1d3a0594ba59696953dfda69d
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-17 14:45:48 -0400

    finished queen and slideable

--- Chess/pieces/queen.rb
+++ Chess/pieces/queen.rb
@@ -1,10 +1,16 @@
-class Queen < Slidable
+require_relative 'piece'
+require_relative 'slideable'
+
+class Queen < Piece
+    include Slideable
 
     def symbol
-        "queen symbol".colorize(color)
+        "♛".colorize(color)
     end
 
-    def move_dirs   #queen's based off of move_dirs module
+    protected
 
+    def move_dirs   #queen's based off of move_dirs module
+        horizonal_and_vertical_dirs + diagonal_dirs
     end
 end
--- Chess/pieces/slideable.rb
+++ Chess/pieces/slideable.rb
@@ -1,4 +1,4 @@
-module Slidable
+module Slideable
     HORIZONTAL_AND_VERTICAL_DIRS = [
         [0,-1],
         [0, 1],
@@ -13,7 +13,7 @@ module Slidable
         [-1, -1]
     ].freeze
 
-    def horizonal_and_vertiacal_dirs
+    def horizonal_and_vertical_dirs
         HORIZONTAL_AND_VERTICAL_DIRS
     end
 
@@ -23,8 +23,8 @@ module Slidable
 
     def move    #holds total array
         moves = [] # all_possible moves
-        move_dirs.each do |x,y|
-            movves << grow_unblocked_move_in_dir(x,y) 
+        move_dirs.each do |x, y|
+            moves << grow_unblocked_move_in_dir(x,y) 
         end
 
         moves
@@ -36,7 +36,7 @@ module Slidable
 # horizonal/vertical
 # [(0...8),y] or [x,(0...8)]
 
-private
+    private
     def move_dirs   #accounts for move and grow methods
         raise NotImplementedError
     end
@@ -45,51 +45,22 @@ private
         possible_moves = []
 
         x, y = pos
-        new_x, new_y = (x + dx), (y +dy)  
-        new_pos = (new_x, new_y)
+        new_x, new_y = (x + dx), (y + dy)  
+        new_pos = [new_x, new_y]
     
-        if (new_pos)board.empty? && valid_pos(new_pos)
-            moves << new_pos
-        elsif !valid_pos(new_pos)
-            break
-        else
-            if new_pos.color != board.pos.color
+        while board.valid_pos?(new_pos)
+            if board.empty?(new_pos) #&& board.valid_pos?(new_pos)
+                possible_moves << new_pos         
+            elsif new_pos.color != board[pos].color
                 possible_moves << new_pos
+            break
             end
         end
-
+        # end
+        
         possible_moves
     end
-    #     end
-    # #     increment piece's current row and by dx dy to generate new position
-    # if the new positions
-    #     1. goes off board
-    #         -stop looping: dont add
-    #     2.empty spot
-    #         -keep looping, add position to array
-    #     3. occupied spot
-    #         - stop looping
-    #         - if opposite color, add position to moves array
-    #         - if same color, ignore positon
-    # return final array
-    # end
-
 end
 
-module Test
-    def move
-        p self
-    end
-end
-
-class Super
-    def initialize
-        @name = "toy"
-    end
-end
-
-class Toy < Super
-    include Test
-end
 
 
