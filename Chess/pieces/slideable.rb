4f89a30a5b382645781ff890d92ba48d7e6dc492
wzheng10 <wzheng100@gmail.com>
2021-03-17 11:25:18 -0700

    Slideable

--- Chess/pieces/slideable.rb
+++ Chess/pieces/slideable.rb
@@ -23,7 +23,11 @@ module Slidable
 
     def move    #holds total array
         moves = [] # all_possible moves
+        move_dirs.each do |x,y|
+            movves << grow_unblocked_move_in_dir(x,y) 
+        end
 
+        moves
     end
 #horizonatal_dir & diagonal_dirs getters 
 #helper method to generate move set of all 
@@ -40,32 +44,29 @@ private
     def grow_unblocked_move_in_dir(dx,dy) 
         possible_moves = []
 
-        x, y = current_pos
-        new_pos = dx, dy
-        newPos = HORIZONTAL_AND_VERTICAL_DIRS.dup    
-        
-        #[(0...8),y] or [x,(0...8)]
-        HORIZONTAL_AND_VERTICAL_DIRS.each do |move|
-            newPos[0] += move[0][0]
-            newPos[1] += move[0][1]
-            possible_moves << newPos
-            
+        x, y = pos
+        new_x, new_y = (x + dx), (y +dy)  
+        new_pos = (new_x, new_y)
+    
+        if (new_pos)board.empty? && valid_pos(new_pos)
+            moves << new_pos
+        elsif !valid_pos(new_pos)
+            break
+        else
+            if new_pos.color != board.pos.color
+                possible_moves << new_pos
+            end
         end
+
         possible_moves
     end
-
-
-    #     end
-    #     while new
-
-
     #     end
     # #     increment piece's current row and by dx dy to generate new position
     # if the new positions
     #     1. goes off board
     #         -stop looping: dont add
     #     2.empty spot
-    #         -keepy looping, add position to array
+    #         -keep looping, add position to array
     #     3. occupied spot
     #         - stop looping
     #         - if opposite color, add position to moves array
@@ -74,3 +75,21 @@ private
     # end
 
 end
+
+module Test
+    def move
+        p self
+    end
+end
+
+class Super
+    def initialize
+        @name = "toy"
+    end
+end
+
+class Toy < Super
+    include Test
+end
+
+
