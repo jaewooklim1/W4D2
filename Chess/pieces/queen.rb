ef7b8e5fb3ee095ab9a3fd9ffa40ac7a0f13a78e
wzheng10 <wzheng100@gmail.com>
2021-03-17 08:31:17 -0700

    Slideable

---
+++ Chess/pieces/queen.rb
@@ -0,0 +1,10 @@
+class Queen < Slidable
+
+    def symbol
+        "queen symbol".colorize(color)
+    end
+
+    def move_dirs   #queen's based off of move_dirs module
+
+    end
+end
---
+++ Chess/pieces/slidable.rb
@@ -0,0 +1,60 @@
+module Slidable
+    HORIZONTAL_AND_VERTICAL_DIRS = [
+        [0,-1],
+        [0,1],
+        [1,0],
+        [-1,0]
+    ].freeze
+
+    DIAGONAL_DIRS = [
+        [1,1], 
+        [1,-1], 
+        [-1,1], 
+        [-1,-1]
+    ].freeze
+
+    def horizonal_and_vertiacal_dirs
+        HORIZONTAL_AND_VERTICAL_DIRS
+    end
+
+    def diagonal_dirs
+        DIAGONAL_DIRS
+    end
+
+    def move    #holds total array
+        moves = [] # all_possible moves
+
+    end
+#horizonatal_dir & diagonal_dirs getters 
+#helper method to generate move set of all 
+# diagonal
+# [1,1], [1,-1], [-1,1] [-1,-1]
+# horizonal/vertical
+# [(0...8),y] or [x,(0...8)]
+
+private
+    def move_dirs   #accounts for move and grow methods
+        raise NotImplementedError
+    end
+
+    def grow_unblocked_move_in_dir(dx,dy) 
+        possible_moves = []
+
+        x, y = current_pos
+        new_pos = dx, dy    #[(0...8),y] or [x,(0...8)]
+        somethinglooping
+        while new
+    #     increment piece's current row and by dx dy to generate new position
+    # if the new positions
+    #     1. goes off board
+    #         -stop looping: dont add
+    #     2.empty spot
+    #         -keepy looping, add position to array
+    #     3. occupied spot
+    #         - stop looping
+    #         - if opposite color, add position to moves array
+    #         - if same color, ignore positon
+    # return final array
+    end
+
+end
